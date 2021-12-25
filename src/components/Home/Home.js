ae3806dd994419501015a04ab32d111008ac2419
mahabub alam <mahabu998@gmail.com>
2021-12-25 11:42:26 +0600

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="text-center">
+      <div><input type="text" className="input_style" /></div>
+      <br></br>
+      <br></br>
+      <input type="submit" className="btn btn-primary" />
+    </div>
+  );
+};
+
+export default Home;
